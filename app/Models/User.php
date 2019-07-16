3acc1fbf2e38271e69090dbb8c922313d641366f
Jorge Aluizio <jaasouza@gmail.com>
2019-07-16 16:03:21 -0300

    Implementação de Autenticação com JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,16 @@
 
 namespace CodeShopping\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
     /**
      * The attributes that are mass assignable.
@@ -28,4 +32,34 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function fill(array $attributes)
+    {
+        !isset($attributes['password']) ? : $attributes['password'] = bcrypt($attributes['password']);
+        return parent::fill($attributes);
+    }
+
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email
+        ];
+    }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use CodeShopping\Models\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +12,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(User::class, 1)
+            ->create([
+                'email' => 'admin@user.com'
+            ]);
+        factory(User::class, 50)
+            ->create();
     }
 }
